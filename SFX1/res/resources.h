6bbc53963a4ebf52f15b1d8fc6f3aebc09646229
iurikali <iurisiecz@gmail.com>
2026-08-12 22:10:14 -0300

    sfx

---
+++ SFX1/res/resources.h
@@ -0,0 +1,11 @@
+#include <genesis.h>
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 sfx_fall[32768];
+extern const u8 sfx_fall_mono[32768];
+extern const SpriteDefinition our_sprite;
+extern const Image bg1;
+extern const Image fg1;
+
+#endif // _RES_RESOURCES_H_
---
+++ SFX1/res/resources.res
@@ -0,0 +1,6 @@
+SPRITE our_sprite "animated sprite 15 16.png" 15 16 FAST 8
+IMAGE bg1 "background1.png" BEST ALL
+IMAGE fg1 "foreground1.png" BEST ALL
+
+WAV sfx_fall "sfx_fall.wav" XGM
+WAV sfx_fall_mono "sfx_fall_mono.wav" XGM
